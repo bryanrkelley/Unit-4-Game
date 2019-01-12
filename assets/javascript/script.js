1f18f57700af2b6e9613ccf530d9fe06a9c08c1d
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-01-11 21:09:15 -0800

    adding functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,75 +1,128 @@
 /*JavaScript*/
-(function() {
-    var targetNumber = 53;
-  
-    $("#number-to-guess").text(targetNumber);
-  
-    var counter = 0;
-  
+(function () {
+    var targetNumbers = [
+        "53",
+        "14",
+        "25",
+        "76",
+        "48",
+        "33"
+    ];
+
+    var counter, targetNumber, wins, losses;
+
+    var $reset = document.getElementById('reset');
+
+
+    function startGame() {
+
+        // document.querySelector("#result").innerHTML = '';
+        // document.querySelector("#guesses").innerHTML = '';
+        // document.querySelector("#guessedLetters").innerHTML = '';
+        // document.querySelector("#word").innerHTML = '';
+
+        // Hide 'play again' button.
+        $reset.classList.add('hide');
+
+        counter = 0;
+        wins = 0;
+        losses = 0;
+
+        // Pick a random word from our array.
+        targetNumber = targetNumbers[Math.floor(Math.random() * targetNumbers.length)]();
+
+        $("#number-to-guess").innerHTML(targetNumber);
+
+        document.querySelector("#word").innerHTML = "Press any key to guess an empty spot.  Be careful though!  You only get 9 chances before it is game over!";
+
+    }
+
     // Now for the hard part. Creating multiple crystals each with their own unique number value.
-  
+
     // We begin by expanding our array to include four options.
     var numberOptions = [10, 5, 3, 7];
-  
+
     // Next we create a for loop to create crystals for every numberOption.
     for (var i = 0; i < numberOptions.length; i++) {
-  
-      // For each iteration, we will create an imageCrystal
-      var imageCrystal = $("<img>");
-  
-      // First each crystal will be given the class ".crystal-image".
-      // This will allow the CSS to take effect.
-      imageCrystal.addClass("crystal-image");
-  
-      // Each imageCrystal will be given a src link to the crystal image
-      imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-  
-      // Each imageCrystal will be given a data attribute called data-crystalValue.
-      // This data attribute will be set equal to the array value.
-      imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-  
-      // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-      $("#crystals").append(imageCrystal);
+
+        // For each iteration, we will create an imageCrystal
+        var imageCrystal = $("<img>");
+
+        // First each crystal will be given the class ".crystal-image".
+        // This will allow the CSS to take effect.
+        imageCrystal.addClass("crystal-image");
+
+        // Each imageCrystal will be given a src link to the crystal image
+        imageCrystal.attr(href="assets/images/Garnet.jpg");
+
+        // Each imageCrystal will be given a data attribute called data-crystalValue.
+        // This data attribute will be set equal to the array value.
+        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+        $("#crystals").append(imageCrystal);
     }
-  
+
     // This time, our click event applies to every single crystal on the page. Not just one.
-    $(".crystal-image").on("click", function() {
-  
-      // Determining the crystal's value requires us to extract the value from the data attribute.
-      // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-      // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-      // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-  
-      var crystalValue = ($(this).attr("data-crystalvalue"));
-      crystalValue = parseInt(crystalValue);
-      // We then add the crystalValue to the user's "counter" which is a global variable.
-      // Every click, from every crystal adds to the global counter.
-      counter += crystalValue;
-  
-      // All of the same game win-lose logic applies. So the rest remains unchanged.
-      alert("New score: " + counter);
-  
-      if (counter === targetNumber) {
-        alert("You win!");
-      }
-  
-      else if (counter >= targetNumber) {
-        alert("You lose!!");
-      }
-  
+    $(".crystal-image").on("click", function (updateNumberOfGuesses) {
+
+        // Determining the crystal's value requires us to extract the value from the data attribute.
+        // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+        // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+        // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        // We then add the crystalValue to the user's "counter" which is a global variable.
+        // Every click, from every crystal adds to the global counter.
+        counter += crystalValue;
+
+        // Lose Logic
+        if (counter >= targetNumber) {
+            document.querySelector("#guesses").innerHTML = "Game Over!";
+            //Congratulate player on trying their best
+            document.querySelector("#word").innerHTML = "The Crystal Gems lost!  Press 'Enter/Return' to play again!";
+            losses++; // increments the losses variable by 1 e.g. losses = losses + 1
+            document.querySelector("#losses").innerHTML = losses;
+        }
+
+        // Win Logic
+        if (counter === targetNumber) {
+            //Congratulate player on guessing correctly
+            document.querySelector("#guesses").innerHTML = "Winner!";
+            document.querySelector("#word").innerHTML = "Good Job!  You saved the day!  Press 'Enter/Return' to play again!";
+            wins++; // increments the wins variable by 1 e.g. wins = wins + 1
+            document.querySelector("#wins").innerHTML = wins;
+        }
+
+        $reset.classList.remove('hide');
+
     });
 
+    // attach handler to the keydown event of the "return" document
+    document.addEventListener('keydown', function handler(e) {
+        if (isValidGuess(e.key)) {
+            handleValidGuess(e);
+        } else if (event.keyCode === 13) {
+            startGame();
+        }
+    });
+
+
+    // Initial start of game
+    startGame();
+
+
+    $reset.addEventListener('click', function handler(e) {
+        // Replay game
+        startGame();
+    });
 
 })();
 
 
-
-
-
-
-
 // jQuery
-$(document).ready(function(){
+$(document).ready(function () {
 
 
 });

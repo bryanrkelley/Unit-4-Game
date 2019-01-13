7c1d151a5c3842e3001b6c7f35f29339a1917756
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-01-12 17:13:13 -0800

    add background and functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,12 @@
 /*JavaScript*/
 (function () {
-    var counter, targetNumber, wins, losses, crystalChoices;
+    var counter,
+        targetNumber,
+        $winner = $("#winner"),
+        $loser = $("#loser"),
+        wins = 0,
+        losses = 0,
+        isRoundDone = false;
 
     // create variables globally towards our application
     var targetNumber = "";
@@ -8,75 +14,83 @@
     var $reset = $("#reset")
 
 
+
     function startGame() {
 
-        // Hide 'play again' button.
-        $reset.addClass("hide")
-
         counter = 0;
-        wins = 0;
-        losses = 0;
+        $("#counter").html(counter);
 
-        // Pick a random number from our array.
+        //Reset the round
+        isRoundDone = false;
+
+        // // Hide 'play again' button.
+        // $reset.addClass("hide");
+
+        //Hide Winner Statement
+        $winner.hide();
+
+        //Hide Loser Statement
+        $loser.hide();
+
         // generate random target number
-        targetNumber = Math.floor((Math.random() * 120) + 19);
-        console.log(targetNumber);
+        targetNumber = Math.floor((Math.random() * 101) + 19);
 
+        //display the value to users
         $("#number-to-guess").html(targetNumber);
 
-        // document.querySelector("#word").innerHTML = "DIRECTIONS!";
-        $("#word").html(targetNumber);
-
-        console.log("your score: " + counter)
-
-        // generate random gem values
-        $("#gem-one").val(Math.floor((Math.random() * 12) + 1));
-        console.log("gem one value is: " + $("#Garnet").val());
-        $("#gem-two").val(Math.floor((Math.random() * 12) + 1));
-        console.log("gem two value is: " + $("#Amethyst").val());
-        $("#gem-three").val(Math.floor((Math.random() * 12) + 1));
-        console.log("gem three value is: " + $("#Pearl").val());
-        $("#gem-four").val(Math.floor((Math.random() * 12) + 1));
-        console.log("gem four value is: " + $("#Steven").val());
+        //Generate random gem values
+        $("#Garnet").val(Math.floor((Math.random() * 12) + 1));
+        console.log("Garnet is: " + $("#Garnet").val());
+        $("#Amethyst").val(Math.floor((Math.random() * 12) + 1));
+        console.log("Amethyst is: " + $("#Amethyst").val());
+        $("#Pearl").val(Math.floor((Math.random() * 12) + 1));
+        console.log("Pearl is: " + $("#Pearl").val());
+        $("#Steven").val(Math.floor((Math.random() * 12) + 1));
+        console.log("Steven is: " + $("#Steven").val());
     }
 
 
     // This time, our click event applies to every single crystal on the page. Not just one.
-    $(".crystal-image").on("click", function (updateNumberOfGuesses) {
+    $(".gem").on("click", function () {
+        if (isRoundDone) {
+            true;
+            return;
+        }
 
-        // var crystalValue = ($(this).attr("data-crystalvalue"));
-        // crystalValue = parseInt(crystalValue);
+        var crystalValue = ($(this).val());
+        crystalValue = parseInt(crystalValue);
+
         // // We then add the crystalValue to the user's "counter" which is a global variable.
         // // Every click, from every crystal adds to the global counter.
-        // counter += crystalValue;
+        counter += crystalValue;
+
+        $("#counter").html(counter);
 
         // Lose Logic
-        if (counter >= targetNumber) {
-            document.querySelector("#guesses").innerHTML = "Game Over!";
+        if (counter > targetNumber) {
             //Congratulate player on trying their best
-            document.querySelector("#word").innerHTML = "The Crystal Gems lost!  Press 'Enter/Return' to play again!";
+            $loser.show();
             losses++; // increments the losses variable by 1 e.g. losses = losses + 1
             document.querySelector("#losses").innerHTML = losses;
+            isRoundDone = true;
         }
 
         // Win Logic
         if (counter === targetNumber) {
             //Congratulate player on guessing correctly
-            document.querySelector("#guesses").innerHTML = "Winner!";
-            document.querySelector("#word").innerHTML = "Good Job!  You saved the day!  Press 'Enter/Return' to play again!";
+            $winner.show();
             wins++; // increments the wins variable by 1 e.g. wins = wins + 1
             document.querySelector("#wins").innerHTML = wins;
+            isRoundDone = true;
         }
 
-        $reset.classList.remove('hide');
+        $reset.removeClass("hide");
 
     });
 
     // attach handler to the keydown event of the "return" document
     document.addEventListener('keydown', function handler(e) {
-        if (isValidGuess(e.key)) {
-            handleValidGuess(e);
-        } else if (event.keyCode === 13) {
+        if (event.keyCode === 13) {
             startGame();
         }
     });
@@ -86,64 +100,8 @@
     startGame();
 
 
-    $reset.addEventListener('click', function handler(e) {
+    $reset.on('click', function (e) {
         // Replay game
         startGame();
     });
 })();
-
-
-// jQuery
-//Not needed since Javascript is at the bottom of the page
-// $(document).ready(function () {
-
-// });
-
-
-
-
-
-
-
-
-
-
-// function playGame() {
-
-//     // add random gen values to your score
-//     $(".gem").on("click", function () {
-
-//         console.log(randomNumber);
-//         gemValue = parseInt($(this).val());
-//         yourScore = parseInt(yourScore + gemValue);
-//         $("#your-score").text(yourScore);
-//         console.log("your score in click: " + yourScore)
-
-//         // if your score equals the random number, you win and the game resets
-//         if (yourScore === randomNumber) {
-//             alert("you win!");
-//             yourWins++;
-//             $("#your-wins").text(yourWins);
-//             console.log("your wins: " + yourWins)
-//             yourScore = 0;
-//             // TODO: find a more efficient way of resetting the "yourScore." Why can't I get it to work in the gameSetup?
-//             $("#your-score").text(yourScore);
-//             initializeGame();
-//             console.log("your wins after initialize: " + yourWins)
-//         }
-
-//         // if your score exceeds the random number, you lose and the game resets
-//         if (yourScore > randomNumber) {
-//             alert("you lost");
-//             yourLosses++;
-//             $("#your-losses").text(yourLosses);
-//             console.log("your losses: " + yourLosses)
-//             yourScore = 0;
-//             $("#your-score").text(yourScore);
-//             initializeGame();
-//             console.log("your losses after intialize: " + yourLosses)
-
-//         }
-//     });
-
-// }
